f772fe4a04abcdb1e7ff349aae10774295e050e7
Jarware, LLC <42174347+jarwarellc@users.noreply.github.com>
2023-08-23 09:54:54 -0400

    Update TrainingCourses.vue

--- TrainingCourses.vue
+++ TrainingCourses.vue
@@ -1,6 +1,7 @@
 <!-- TrainingCourses.vue -->
 <template>
   <div>
+    <!-- JarWare Developer: Display a list of training courses -->
     <h1>Training Courses</h1>
     <ul>
       <li><router-link to="/purchase-training">Purchase Training</router-link></li>
@@ -13,8 +14,22 @@
 <!-- PurchaseTraining.vue -->
 <template>
   <div>
+    <!-- JarWare Developer: Display purchase training details and quiz submission button -->
     <h2>Purchase Training for Card Holders</h2>
     <p>Complete the quiz to earn your certificate.</p>
-    <!-- ...quiz components -->
+    <button @click="submitQuiz">Submit Quiz</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    submitQuiz() {
+      // JarWare Developer: Simulate quiz submission and certificate issuance
+      setTimeout(() => {
+        alert('Congratulations! You have completed the quiz.');
+      }, 1000);
+    },
+  },
+};
+</script>
