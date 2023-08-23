5225ace65ed516a09be5643e2a16d46fb18fd9ff
Jarware, LLC <42174347+jarwarellc@users.noreply.github.com>
2023-08-23 09:49:53 -0400

    Create TrainingCourses.vue

---
+++ TrainingCourses.vue
@@ -0,0 +1,20 @@
+<!-- TrainingCourses.vue -->
+<template>
+  <div>
+    <h1>Training Courses</h1>
+    <ul>
+      <li><router-link to="/purchase-training">Purchase Training</router-link></li>
+      <li><router-link to="/travel-training">Travel Training</router-link></li>
+      <!-- ...other course links -->
+    </ul>
+  </div>
+</template>
+
+<!-- PurchaseTraining.vue -->
+<template>
+  <div>
+    <h2>Purchase Training for Card Holders</h2>
+    <p>Complete the quiz to earn your certificate.</p>
+    <!-- ...quiz components -->
+  </div>
+</template>
