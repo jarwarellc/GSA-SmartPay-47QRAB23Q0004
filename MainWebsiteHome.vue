a7806a25329c56d5c0ec5fbcae1372af577ba303
Jarware, LLC <42174347+jarwarellc@users.noreply.github.com>
2023-08-23 09:53:42 -0400

    Update MainWebsiteHome.vue

--- MainWebsiteHome.vue
+++ MainWebsiteHome.vue
@@ -1,6 +1,7 @@
 <!-- MainWebsiteHome.vue -->
 <template>
   <div>
+    <!-- JarWare Developer: Display a welcome message and link to Section 889 tool -->
     <h1>Welcome to GSA SmartPay</h1>
     <p>Discover the latest information on state sales tax and more.</p>
     <router-link to="/section-889">Section 889 Representations Tool</router-link>
