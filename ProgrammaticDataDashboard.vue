c497963a627dc1f6a58b3695b275480e57298bd0
Jarware, LLC <42174347+jarwarellc@users.noreply.github.com>
2023-08-23 09:51:44 -0400

    Create ProgrammaticDataDashboard.vue

---
+++ ProgrammaticDataDashboard.vue
@@ -0,0 +1,31 @@
+<!-- ProgrammaticDataDashboard.vue -->
+<template>
+  <div>
+    <h1>Programmatic Data Dashboard</h1>
+    <p>Explore agency spend data and trends.</p>
+    <button @click="fetchData">Fetch Data</button>
+    <div v-if="dataFetched">
+      <!-- Display data visualization components -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataFetched: false,
+      // ...other data properties
+    };
+  },
+  methods: {
+    fetchData() {
+      // Simulate fetching data from an API
+      setTimeout(() => {
+        this.dataFetched = true;
+        // ...populate data properties with fetched data
+      }, 1000);
+    },
+  },
+};
+</script>
